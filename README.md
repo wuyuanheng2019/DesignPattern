fd277d3db2fd0cfcb8acefda4f8b8d9993acd214
wyh <16619925116@163.com>
2019-04-26 17:49:34 +0800

    策略模式

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 1、策略模式----strategy
 2、工厂设计模式----factory
 3、装饰者设计模式----decorating 
+4、代理模式----agent
--- src/main/java/com/wyh/designpattern/strategy/entity/Order.java
+++ src/main/java/com/wyh/designpattern/strategy/entity/Order.java
@@ -10,6 +10,7 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Order {
+
     /**
      * 订单编号
      */
