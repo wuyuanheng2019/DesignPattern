529157c6d787947a37fabf5c231cae21a8fc2f87
wyh <16619925116@163.com>
2019-04-28 14:04:00 +0800

    启动类

--- src/main/java/com/wyh/designpattern/DesignpatternApplication.java
+++ src/main/java/com/wyh/designpattern/DesignpatternApplication.java
@@ -7,6 +7,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableSwagger2
 public class DesignpatternApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(DesignpatternApplication.class, args);
     }
