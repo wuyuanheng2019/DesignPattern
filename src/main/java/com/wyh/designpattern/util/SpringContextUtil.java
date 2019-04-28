034ec076021dbb287db3bd2a43bf456f8e236ea6
wyh <16619925116@163.com>
2019-04-28 14:03:19 +0800

    工具类

--- src/main/java/com/wyh/designpattern/util/SpringContextUtil.java
+++ src/main/java/com/wyh/designpattern/util/SpringContextUtil.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class SpringContextUtil implements ApplicationContextAware {
+
     private static ApplicationContext applicationContext;
 
     /**
