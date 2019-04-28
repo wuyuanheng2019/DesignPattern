79a1d329d7a754aee7547013c71262db5a5c52a1
wyh <16619925116@163.com>
2019-04-28 18:34:14 +0800

    模板方法模式

--- src/main/java/com/wyh/designpattern/template/Play.java
+++ src/main/java/com/wyh/designpattern/template/Play.java
@@ -3,6 +3,7 @@ package com.wyh.designpattern.template;
 public class Play {
     public static void main(String[] args) {
 
+
         //在这里不同的游戏实现相同的方法，处理不同的逻辑代码
         Football football = new Football();
         football.initialize();
